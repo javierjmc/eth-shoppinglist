c6efe418cb7508ce0d4b9d96e6e7741e9b9ca89c
Javier Mendonça <javier.mendonca.costa@hemnet.se>
2020-07-09 11:09:51 +0200

    Adding ropsten config

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,6 @@
+require('dotenv').config()
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
 module.exports = {
   // Uncommenting the defaults below
   // provides for an easier quick-start with Ganache.
@@ -6,10 +9,17 @@ module.exports = {
   // for more details on how to specify configuration options!
   //
   networks: {
-   ganache: {
+   development: {
      host: "127.0.0.1",
      port: 7545,
      network_id: "*"
    },
+   ropsten: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, process.env.ROPSTEN_URL)
+      },
+      network_id: 3,
+      gas: 4000000      //make sure this gas allocation isn't over 4M, which is the max
+    }
   }
 };
